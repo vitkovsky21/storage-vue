6c4c873789f627b57385635783b9df58281c1adf
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2023-03-17 14:33:23 +0300

    local json

--- http-common.ts
+++ http-common.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://vitkovsky21.github.io/storage-vue/data.json',
+  baseURL: './data.json',
   responseType: 'json',
   headers: {
     'Content-type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
   }
 })
