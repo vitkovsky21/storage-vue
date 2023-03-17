803d5d305211980dfd93fd9148a12a78bff75687
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2023-03-17 14:17:21 +0300

    headers axios issues fixed

--- http-common.ts
+++ http-common.ts
@@ -1,9 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default axios.create({
-  baseURL: "./data.json",
+  baseURL: 'http://vitkovsky21.github.io/storage-vue/data.json',
   responseType: 'json',
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
   }
-});
+})
